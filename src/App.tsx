ccd47a6a32b545da56a66967a1243a56ca4e79d7
boomtnt2843 <boomphat@odds.team>
2024-09-30 11:47:19 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import OTPInput from "react-otp-input";
+import "./App.css";
 
 function App() {
+  const [otp, setOtp] = useState("");
+  useEffect(() => {
+    console.log("otp::: " + otp);
+    
+  },[otp,setOtp])
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +24,22 @@ function App() {
         >
           Learn React
         </a>
+        <div className="p-2 text-black gap-1">
+          <OTPInput
+            value={otp}
+            onChange={setOtp}
+            numInputs={6}
+            inputStyle={{
+              marginLeft: '8px',
+              width: '60px',
+              height: '60px',
+              borderRadius: '4px',
+              border: '1px solid #32438d',
+            }}
+            inputType="tel"
+            renderInput={(props) => <input {...props} />}
+          />
+        </div>
       </header>
     </div>
   );
